38a2d48d7a033d57a50019b7f608fb6731a839e0
GarethHillston <gareth.hillston@gmail.com>
2017-10-22 15:17:32 +0100

    initial push

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+
+app.get('/',function(req,res){
+    res.sendFile(__dirname+'/index.html');
+});
+
+server.listen(process.env.PORT || 8081,function(){
+    console.log('Listening on '+server.address().port);
+});
